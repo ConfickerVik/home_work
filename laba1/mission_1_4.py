a0e38ebac446f6ed67de43bb568de52f1f837b8c
Viktor <oshepkoviktor@gmail.com>
2019-09-13 19:00:36 +0800

    Подсчёт суммы(произведения) аргументов из консоли

--- laba1/mission_1_4.py
+++ laba1/mission_1_4.py
@@ -1,21 +1,29 @@
 import sys
 
 
-class Result:
-    def sum(self, arg1, arg2):
-        total_sum = arg1 + arg2
+class Result_sum:
+    def sum(self, total_sum, arg):
+        total_sum += arg
         return total_sum
 
-    def multiply(self, arg1, arg2):
-        total_mul = arg1 * arg2
+
+class Result_mul:
+    def multiply(self, total_mul, arg):
+        total_mul *= arg
         return total_mul
 
 
 if __name__ == "__main__":
-    argument_1 = int(sys.argv[1])
-    argument_2 = int(sys.argv[2])
-    obj = Result()
-    result_sum = obj.sum(argument_1, argument_2)
-    result_mul = obj.multiply(argument_1, argument_2)
-    print("Сумма двух целочисленных аргументов равна: ", result_sum,
-          "\nПроизведение двух целочисленных аргуметов равна: " ,result_mul)
+    argv = sys.argv
+    obj_sum = Result_sum()
+    obj_mul = Result_mul()
+    result_sum = 0
+    result_mul = 1
+    try:
+        for argument in range(1, len(argv)):
+            result_sum = obj_sum.sum(result_sum, int(argv[argument]))
+            result_mul = obj_mul.multiply(result_mul, int(argv[argument]))
+        print("Сумма целочисленных аргументов равна: ", result_sum,
+              "\nПроизведение целочисленных аргуметов равна: " ,result_mul)
+    except ValueError:
+        print("Неверно введены данные!")
