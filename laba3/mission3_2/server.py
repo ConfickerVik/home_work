a1f773739e13446079765cd25c90d5d33079b64e
Viktor <43670422+ConfickerVik@users.noreply.github.com>
2019-09-28 09:11:28 +0800

    Сервер

---
+++ laba3/mission3_2/server.py
@@ -0,0 +1,24 @@
+import socket
+import random
+
+
+def getstr():
+    answer = ['Салам алейкум','Привіт','Dobry den','Konnichi wa','Здаров','Guten Tag']
+    return data + random.choice(get)
+
+sock = socket.socket()
+sock.bind(('', 9090))
+sock.listen(10)
+conn, addr = sock.accept()
+
+print('connected:', addr)
+
+while True:
+    data = conn.recv(1024)
+    if not data:
+        break
+    else:
+        mes = getstr(data)
+        conn.send(mes.encode())
+
+conn.close()
