0baefdf36a14260f2ef8c83a37bb55351bf78112
Viktor <oshepkoviktor@gmail.com>
2019-11-01 20:13:47 +0800

    Исполнительный файл

--- laba7/synchronized/synchro.py
+++ laba7/synchronized/synchro.py
@@ -1,30 +1,48 @@
+import logging
+import random
 import threading
 import time
-import random
 
-semaphore = threading.Semaphore(0)
-
-
-def tunnel():
-    print("Туннель свободен!")
-    semaphore.acquire()
-    print(f"Поезд прошёл за {item} секунд(ы)")
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s (%(threadName)-2s) %(message)s',
+                    )
 
 
-def train():
-    global item
-    item = random.randint(0, 10) + 1
-    print("Поезд заехал в туннель! Ожидание")
-    time.sleep(item)
-    semaphore.release()
+class ActiveTonnel(object):
+    def __init__(self):
+        super(ActiveTonnel, self).__init__()
+        self.active = []
+        self.lock = threading.Lock()
+
+    def makeActive(self, name):
+        with self.lock:
+            self.active.append(name)
+            logging.debug('Заехал в тонель поезд номер %s', self.active[-1])
+
+    def makeInactive(self, name):
+        with self.lock:
+            try:
+                if len(self.active) == 2:
+                    logging.debug('Выехал из тонеля поезд номер %s', self.active[0])
+                else:
+                    logging.debug('Выехал из тонеля поезд номер %s', self.active[-1])
+            except IndexError:
+                logging.debug('Поезда закончились, очередь пуста')
+            self.active.remove(name)
 
 
-if __name__ == '__main__':
-    for i in range(10):
-        t1 = threading.Thread(target=tunnel)
-        tr = threading.Thread(target=train)
-        t1.start()
-        tr.start()
-        t1.join()
-        tr.join()
-    print("Finish.")
+def train(s, pool):
+    logging.debug('Ожидание очереди в тонель')
+    with s:
+        name = threading.currentThread().getName()
+        pool.makeActive(name)
+        time.sleep(0.1)
+        pool.makeInactive(name)
+
+
+tonnel = ActiveTonnel()
+s = threading.Semaphore(2)
+for i in range(1, 11):
+    t = threading.Thread(target=train, name=str(i), args=(s, tonnel))
+    t.start()
+
